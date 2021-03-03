46f94a454088031676bf6415b1cc0a359edf75d7
Brody Bayley <brodybayley@gmail.com>
2021-03-03 22:27:14 +0000

    add properties by city and most visited cities queries

---
+++ 1_queries/3_properties_by_city.sql
@@ -0,0 +1,9 @@
+-- Show all details about properties located in a city including rating
+SELECT properties.*, avg(rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
