559a592c5705718924da39bbecd14d2998951ac1
Brody Bayley <brodybayley@gmail.com>
2021-03-05 01:50:11 +0000

    clean file and delete duplicate code

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -156,8 +156,6 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  console.log(queryString, queryParams);
-
   return pool.query(queryString, queryParams)
     .then(res => res.rows);
 };
@@ -174,23 +172,12 @@ exports.getAllProperties = getAllProperties;
 const addProperty = function (property) {
   return pool.query(`
   INSERT INTO properties (
-    title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+    owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
     VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;
-  `, [property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
     .then(res => res.rows[0]);
 };
 
 exports.addProperty = addProperty;
-
-const addUser = function (user) {
-  return pool.query(`
-  INSERT INTO users (
-    name, email, password) 
-    VALUES (
-    $1, $2, $3)
-    RETURNING *;
-  `, [user.name, user.email, user.password])
-    .then(res => res.rows[0]);
-};
